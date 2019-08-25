9dc01254c42c38c1f5ef895222951ffc860a6758
Sam Morrell <samuelmorrell@gmail.com>
2019-08-26 00:28:28 +0100

    Fixed bug where logo was hidden by menu. Menu and logo now correctly scale and layout.

--- parts/menu.php
+++ parts/menu.php
@@ -1,19 +1,20 @@
 <div class='width-sizer' id='header-container'>
 	<a id='menu-logo-container' href='<?php echo get_site_url(); ?>'>
-
 	</a>
-	<div id='header-menu-container'>
-		<?php wp_nav_menu( 
-			array( 
-				'theme_location' => 'header',
-				'container' => false,
-				'menu_id' => 'header-main-menu',
-			)
-		); ?>
-	
-		<!-- Let's include some sidereal clock icon and search button goodness -->
-		<label for='search-modal-toggle' id='search-icon' class='menu-item display-on-mobile'>
-			<i class="fa fa-search"></i>
-		</label>
+	<div id='header-menu-spacer'>
+		<div id='header-menu-container'>
+			<?php wp_nav_menu( 
+				array( 
+					'theme_location' => 'header',
+					'container' => false,
+					'menu_id' => 'header-main-menu',
+				)
+			); ?>
+			
+			<!-- Let's include some sidereal clock icon and search button goodness -->
+			<label for='search-modal-toggle' id='search-icon' class='menu-item display-on-mobile'>
+				<i class="fa fa-search"></i>
+			</label>
+		</div>
 	</div>
 </div>
