edce039829fc0e1c94b530ad0a5d9c1b1cc6c056
Sam Morrell <samuelmorrell@gmail.com>
2022-09-19 21:19:50 +0100

    First pass of new design.
    I am now happy with the pass of the front page and I have got the basic page layout a little more down.

--- parts/home-header.php
+++ parts/home-header.php
@@ -9,17 +9,21 @@
 	<div class="container header-container">
 		<div class="header-vertical-spacer container"></div>
 			<div class='row'>
-				<div class='col-6'>
+				<div class='col-12 col-md-6'>
 					<div class='home-header-sub-heading'>
 						<div class='home-header-uoe-text'>University of Exeter's</div>
 						<div class='home-header-space-science-text'>Space Science &amp; Astronomy</div>
 						<div class='home-header-soc-text'>Student Society</div>
 					</div>
 				</div>
-				<div class='col-6'>
+				<div class='col-12 col-md-6'>
 					<div class="header-logo-container"><img src="<?php echo get_template_directory_uri(); ?>/img/space-exe-logo-2020.png" class="header-logo"></div>
 				</div>
 			</div>
 		</div>
+
+		<div class='main-content-container'>
+		</div>
+
 	</div>
 </header>
--- parts/page-container.php
+++ parts/page-container.php
@@ -1,15 +1,16 @@
-<div class='container' >
+<div class='page-container'>
+	<div class='container' id='page-container-offset'>
 
-	<!-- Page Title --> 
-	<div class='page-title-container'>
-		<h2 class='page-title'>
-			<?php echo get_the_title(); ?>
-		</h2>
-	</div>
+		<!-- Page Title --> 
+		<div class='page-title-container'>
+			<h2 class='page-title'>
+				<?php echo get_the_title(); ?>
+			</h2>
+		</div>
 
-	<!-- Main Page Content -->
-	<div class='page-content-container'>
-		<?php the_content(); ?>
+		<!-- Main Page Content -->
+		<div class='page-content-container'>
+			<?php the_content(); ?>
+		</div>
 	</div>
 </div>
-
