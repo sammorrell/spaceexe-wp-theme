e18f7201a9a55ca71326d5c965ff2c8575148513
Sam Morrell <samuelmorrell@gmail.com>
2024-09-17 23:23:44 +0100

    Corrected alignment of homepage content.
    This has been bothering me for years, and I've finally found a moment to fix it. Yay!

--- front-page.php
+++ front-page.php
@@ -2,10 +2,12 @@
 <div id='footer-spacer'>
 <?php get_template_part('parts/home-header'); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part('parts/homepage-container'); ?>
-	<?php endwhile; endif; ?>
-	<div style='width: 100%;'></div>
+<div class='main-content-container container'>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part('parts/homepage-container'); ?>
+		<?php endwhile; endif; ?>
+		<div style='width: 100%;'></div>
 
+	</div>
 </div>
 <?php get_footer(); ?>
